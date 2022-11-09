9694204dadb7074e49d6719f428dc359df880686
empire-uts <kii.einsatz@docomo.ne.jp>
2022-11-09 09:43:52 +0900

    pushmiss? fix

---
+++ components/topBar.tsx
@@ -0,0 +1,17 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+import Header from "./organisms/header";
+
+export default function TopBar(props: any) {
+  const router = useRouter();
+  const selectedScreen = router.pathname.replace(/[/]/g, "");
+  return (
+    <Header
+      selectedScreen={selectedScreen}
+      imgUrl={props.imgUrl}
+      idList={props.idList}
+      setActingAccount={props.setActingAccount}
+    />
+  );
+}
